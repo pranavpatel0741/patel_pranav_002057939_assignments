b09d418a0601ff073e6e40f2b89c89c209e031bf
pranavpatel0741 <patel.pranavna@northeastern.edu>
2024-09-19 18:35:53 -0400

    Add files via upload

---
+++ Assignments_1/src/model/Product.java
@@ -0,0 +1,179 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author pranavpatel
+ */
+public class Product {
+
+    public String getFName() {
+        return FName;
+    }
+
+    public void setFName(String FName) {
+        this.FName = FName;
+    }
+
+    public String getLName() {
+        return LName;
+    }
+
+    public void setLName(String LName) {
+        this.LName = LName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getDLNum() {
+        return DLNum;
+    }
+
+    public void setDLNub(String DLNum) {
+        this.DLNum = DLNum;
+    }
+
+    public String getDLstate() {
+        return DLState;
+    }
+
+    public void setDLstate(String DLstate) {
+        this.DLState = DLstate;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getADL1() {
+        return ADL1;
+    }
+
+    public void setADL1(String ADL1) {
+        this.ADL1 = ADL1;
+    }
+
+    public String getADL2() {
+        return ADL2;
+    }
+
+    public void setADL2(String ADL2) {
+        this.ADL2 = ADL2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getZip() {
+        return Zip;
+    }
+
+    public void setZip(String Zip) {
+        this.Zip = Zip;
+    }
+
+    public String getTeleNum() {
+        return TeleNum;
+    }
+
+    public void setTeleNum(String TeleNum) {
+        this.TeleNum = TeleNum;
+    }
+
+    public String getFaxNum() {
+        return FaxNum;
+    }
+
+    public void setFaxNum(String FaxNum) {
+        this.FaxNum = FaxNum;
+    }
+   
+
+    public String getOccu() {
+        return Occu;
+    }
+
+    public void setOccu(String Occu) {
+        this.Occu = Occu;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+     String FName;
+    String LName;
+    String Email;
+    String Phone;
+    String DLNum;
+    String DLState;
+    String SSN;
+    String ADL1;
+    String ADL2;
+    String City;
+    String State;
+    String Zip;
+    String TeleNum;
+    String FaxNum;
+    String Occu;
+    String DOB;
+    String Country;
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+    String Gender;
+
+  
+   
+    
+}
